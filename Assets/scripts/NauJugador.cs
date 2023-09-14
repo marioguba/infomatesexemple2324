9c2139d1636cb49eb7a50f641f8d76a1bdbee1db
Mario Guirao Barreiro <marioguirao_@ceroca.cat>
2023-09-14 19:43:37 +0200

    Moviment Horitzontal Nau

---
+++ Assets/scripts/NauJugador.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NauJugador : MonoBehaviour
+{
+    public float _velNau;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _velNau = 5f;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direccioHoritzontal = Input.GetAxisRaw("Horizontal");
+        //Debug.Log(direccioHoritzontal);
+
+        Vector2 direccioIndicada = new Vector2(direccioHoritzontal,0f);
+
+        Vector2 novaPos = transform.position; // Ens retorna la posició actual de la nau.
+        novaPos += direccioIndicada *    _velNau * Time.deltaTime;
+        transform.position = novaPos;
+    }
+}
